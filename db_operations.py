717370170ca35b2f1b6115fea3937724c234baa8
Adam Huybers <70770610+Hooobers@users.noreply.github.com>
2022-11-24 14:48:08 -0600

    Expanded functionality

--- db_operations.py
+++ db_operations.py
@@ -1,45 +1,79 @@
-import sqlite3
+'''
+Name: 
+Program: Business Information Technology
+Course: ADEV-3005 Programming in Python
+Created: 2022-11-20
+Updated: 2022-11-24
+
+TODO - All database operations.
+
+Final Project
+ 
+@author Adam Huybers [ahuybers@academic.rrc.ca]
+@version 1.0.3
+'''
+from dbcm import DBCM
 
 
 class DBOperations():
     """Database operations."""
-    def __init__(self):
+    def __init__(self, db):
         """Init function."""
-        try:
-            self.conn = sqlite3.connect("weather.sqlite")
-            self.c = self.conn.cursor()
-        except Exception as e:
-            print("Error opening DB:", e)
+        self.db = db
 
-    # Work in progress.
-    def fetch_data(self, sql):
+    def fetch_data(self, first, last, month):
         """Fetch data from the database."""
         try:
-            self.c.execute(sql)
-            return self.c.fetchall()
+            # Context manager used to handle connections.
+            with DBCM(self.db) as cm_cursor:
+                if month:
+                    first_date = first + '-01'
+                    last_date = last + '-31'
+                else:
+                    first_date = first
+                    last_date = last
+
+                # Command execution.    
+                cm_cursor.execute(f"select sample_date, avg_temp from samples where sample_date between '{first_date}' and '{last_date}' order by date(sample_date)")
+                return [dict(row) for row in cm_cursor.fetchall()]
+
         except Exception as e:
             print("Error fetching data:", e)
 
-    # Work in progress.
-    def save_data(self, sql, data):
+    def save_data(self, data, month, year):
         """Save data to the database."""
         try:
-            self.c.execute(sql, data)
-            self.conn.commit()
+            command = """insert into samples (sample_date, location, max_temp, min_temp, avg_temp)values (?,?,?,?,?)"""
+
+            for day, temps in data.items():
+                try:
+                    set_data = list()
+                    set_data.append(f"{year}-{month}-{day}")
+                    set_data.append("Winnipeg")
+
+                    for key, value in temps.items():
+                        set_data.append(value)
+
+                    with DBCM(self.db) as cm_cursor:
+                        cm_cursor.execute(command, set_data)
+
+                except Exception as e:
+                    print("Error saving data:", e)
         except Exception as e:
             print("Error saving data:", e)
 
     def initialize_db(self):
         """Initialize the database."""
         try:
-            self.c.execute("""create table samples
-                            (id integer primary key autoincrement not null,
-                            sample_date text not null,
-                            location text not null,
-                            min_temp real not null,
-                            max_temp real not null,
-                            avg_temp real not null);""")
-            self.conn.commit()
+            # Context manager used to handle connections.
+            with DBCM(self.db) as cm_cursor:
+                cm_cursor.execute("""create table if not exists samples
+                                 (id integer primary key autoincrement not null,
+                                  sample_date text not null,
+                                  location text not null,
+                                  min_temp real not null,
+                                  max_temp real not null,
+                                  avg_temp real not null);""")
             print("Table created successfully.")
         except Exception as e:
             print("Error creating table:", e)
@@ -47,8 +81,9 @@ class DBOperations():
     def purge_data(self):
         """Purge data from the database."""
         try:
-            self.c.execute("delete from samples")
-            self.conn.commit()
+            # Context manager used to handle connections.
+            with DBCM(self.db) as cm_cursor:
+                cm_cursor.execute("delete from samples")
             print("Data purged successfully.")
         except Exception as e:
             print("Error purging data:", e)
--- dbcm.py
+++ dbcm.py
@@ -1,15 +1,32 @@
+'''
+Name: 
+Program: Business Information Technology
+Course: ADEV-3005 Programming in Python
+Created: 2022-11-24
+Updated: 2022-11-24
+
+TODO - Context manager for database connections.
+
+Final Project
+ 
+@author Adam Huybers [ahuybers@academic.rrc.ca]
+@version 1.0.0
+'''
 import sqlite3
+
 
 class DBCM():
     """Database connection manager."""
-    def __init__(self, path):
-        self.path = path
+    def __init__(self, db):
+        self.db = db
 
     def __enter__(self):
-        self.conn = sqlite3.connect(self.path)
+        self.conn = sqlite3.connect(self.db)
+        self.conn.row_factory = sqlite3.Row
         self.cursor = self.conn.cursor()
         return self.cursor
 
     def __exit__(self, exc_type, exc_value, exc_trace):
         self.conn.commit()
         self.conn.close()
+        self.cursor.close()
